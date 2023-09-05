434f28be19c2efebf91471d322f70ed2b2be4991
AphiweSeduana <110010495+AphiweSeduana@users.noreply.github.com>
2023-09-05 08:46:21 +0200

    Delete assets directory

--- assets/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import StartNavigations from "./src/navigations/StartNavigations";
-
-export default function App() {
-  return <StartNavigations />;
-}
--- assets/src/assets/Colors.js
+++
@@ -1,8 +0,0 @@
-export const Colors = {
-  gray: "#E3E9EC",
-  black: "#131212",
-  darkGray: "#BFBFBF",
-  white: "#fff",
-  blue: "#2F9AAA",
-  lightBlack: "#2B2B2B",
-};
